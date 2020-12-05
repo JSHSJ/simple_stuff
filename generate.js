8e0332e63cc0d53aa5b3800fc18b479c025f8e19
Joshua <joshua.stuebner@bloxxter.com>
2020-12-05 09:56:33 +0100

    more pages

---
+++ generate.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+
+console.log('Hallo. Ich mach mal.');
+
+const pages = fs.readdirSync('pages');
+const links = pages
+  .filter((p) => !p.startsWith('.'))
+  .map((page) => {
+    const content = fs.readFileSync(`pages/${page}/index.html`, { encoding: 'utf8' });
+    const title = content.split('<title>')[1].split('</title>')[0];
+    // return {
+    //   target: `pages/${page}`,
+    //   name: title,
+    // };
+    return `<a href="pages/${page}">${title}</a>`;
+  });
+
+const index = fs.readFileSync('index.template', { encoding: 'utf8' }).replace('__LINKS__', links.join('\n'));
+fs.writeFileSync('index.html', index);
